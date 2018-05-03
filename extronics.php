b223fc26132f31c00e444688b260023645728251
jugabratnath <38959523+jugabratnath@users.noreply.github.com>
2018-05-03 11:33:56 -0700

    Add files via upload

---
+++ extronics.php
@@ -0,0 +1,29 @@
+<?php session_start(); ?>
+<html>
+<head>
+<title>
+new tab
+</title>
+</head>
+<body bgcolor="black" text="red" text align="center">
+<h1><center>MTJAD</center></h1><br><br>
+<hr></hr>
+<h1><center>fill up the instruction</center></h1>
+<form action="dataa11.php" method="POST" >
+<b>name</b>
+<input type ="text" name="name"><br><br>
+phone no
+<input type="text" name="phone"><br><br>
+address
+<input type="int" name="add"><br><br>
+<h3><center>fill the product name which you want</center></h3><br>
+<b>product</b>
+<input type="text" name="product_name"><br><Br>
+
+<input type="submit" value="login">
+<h2><b>about our product</b></h2> 
+</form> 
+
+</body>
+</html>
+
